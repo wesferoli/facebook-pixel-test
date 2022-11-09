9f20f2e09f33f994f198ef293289c337eeb2a4f4
wesferoli <wesleyfer.oli97@gmail.com>
2022-11-09 16:20:51 -0300

    first tests with facebook pixel

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
+import { useEffect } from "react";
+import ReactPixel from "react-facebook-pixel";
 import "./App.scss";
 
 type PaymentType = "cc" | "pix" | "billet";
 
+const options = {
+  autoConfig: true,
+  debug: false,
+};
+const pixel = "821778102203144";
+
 function App() {
+  ReactPixel.init(pixel, undefined, options);
+  ReactPixel.trackSingle(pixel, "InitiateCheckout");
+
   const sendEvent = (paymentType: PaymentType) => {
     if (paymentType === "cc") {
       console.log(paymentType);
+      ReactPixel.trackSingle(pixel, "Purchase", {
+        currency: "BRL",
+        value: 10.0,
+      });
     } else if (paymentType === "pix") {
       console.log(paymentType);
+      ReactPixel.trackSingle(pixel, "Purchase", {
+        currency: "BRL",
+        value: 20.0,
+      });
     } else if (paymentType === "billet") {
       console.log(paymentType);
+      ReactPixel.trackSingle(pixel, "Purchase", {
+        currency: "BRL",
+        value: 30.0,
+      });
     }
   };
+
+  // useEffect(() => {
+  //   fbq('track', 'InitiateCheckout')
+  // }, [])
 
   return (
     <div className="App">
